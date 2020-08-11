018e4aeec0a3927b9ad094df8118990834ee28f1
Han Xu <hanx-2011@hotmail.com>
2020-08-10 21:12:43 -0700

    exploratory analysis and plot

--- explore.py
+++ explore.py
@@ -5,3 +5,33 @@ Created: August 10, 2020
 Program for exploring the given dataset.
 Outputs and plots the data to extract meaningful insight.
 """
+import matplotlib.pyplot as plt
+
+from data import *
+
+if __name__ == '__main__':
+    try:
+        # reads and outputs head of dataframe to terminal
+        df_msg = read_data('raw_text.txt')
+        print('Original dataframe:')
+        print(df_msg.head())
+
+        print('\n')
+
+        # checks number of instances for each language
+        print(f'Number of Dutch text: {sum(df_msg["label"] == "nl")}')
+        print(f'Number of English text: {sum(df_msg["label"] == "en")}')
+
+        print('\n')
+
+        # explores difference in length between en and nl text
+        df_msg['length'] = df_msg['message'].apply(len)
+        # plots text length histogram
+        print('Plotting histogram of text length by text language...')
+        fig = plt.figure()
+        df_msg.hist(column='length', by='label', bins=50, figsize=(12, 4))
+        plt.savefig('txt_len_hist.png')
+        print('Saved plot to txt_len_hist.png')
+
+    except Exception as e:
+        print(f'could not execute script: {e}')
